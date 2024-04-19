fe67702825c076895ab55e6d225be7d9d56e042f
Daniel Crompton <danielcrompton5@gmail.com>
2024-04-19 21:04:39 +0100

    Initial Commit

---
+++ Project Tracker/Project_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_TrackerApp.swift
+//  Project Tracker
+//
+//  Created by Daniel Crompton on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
