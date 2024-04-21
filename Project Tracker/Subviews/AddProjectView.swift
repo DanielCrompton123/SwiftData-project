f6f307b5faaf391215d27f68ae33a30ee6e772e6
Daniel Crompton <danielcrompton5@gmail.com>
2024-04-21 12:23:05 +0100

    Add the project made in the Add project view to SwiftData

--- Project Tracker/Subviews/AddProjectView.swift
+++ Project Tracker/Subviews/AddProjectView.swift
@@ -6,10 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddProjectView: View {
     
-    var project: Project?
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+    
+    var project: Project
     @State private var projectName = ""
     
     var body: some View {
@@ -34,6 +38,10 @@ struct AddProjectView: View {
                 
                 Button(action: {
                     // Save the project to SwiftData
+                    project.name = projectName
+                    context.insert(project)
+                    
+                    dismiss()
                 }, label: {
                     ZStack {
                         Capsule()
